3d3e4fdfaf4054d8e661169ea4a14d0d38b39ada
Vasaventura <vasily.zhuravlev@unine.ch>
2020-11-04 15:16:08 +0100

    Improved text position in the Start screen

--- dodger.py
+++ dodger.py
@@ -74,9 +74,9 @@ baddieImage = pygame.image.load('gremlin.png')
 
 # Show the "Start" screen.
 windowSurface.fill(MENUBACKGROUNDCOLOR)
-drawText('Dodger', font, windowSurface, (WINDOWWIDTH / 3), (WINDOWHEIGHT / 3))
-drawText('Press a key to start', font, windowSurface, (WINDOWWIDTH / 3) - 80, (WINDOWHEIGHT / 3) + 50)
-drawText('saving Christmas', font, windowSurface, (WINDOWWIDTH / 3) - 70, (WINDOWHEIGHT / 3) + 100)
+drawText('X-Mas Dodger', font, windowSurface, (WINDOWWIDTH / 3), (WINDOWHEIGHT / 3))
+drawText('Press a key to start', font, windowSurface, (WINDOWWIDTH / 3)-40, (WINDOWHEIGHT / 3) + 50)
+drawText('saving Christmas', font, windowSurface, (WINDOWWIDTH / 3)-35, (WINDOWHEIGHT / 3) + 100)
 pygame.display.update()
 waitForPlayerToPressKey()
 
