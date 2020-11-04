ee305a100e79f245194c72bbb7b36f83e982dcbe
FlaviaMeylan <flavia.meylan@unine.ch>
2020-11-04 09:25:36 +0100

    Changed Mario to Noël

--- dodger.py
+++ dodger.py
@@ -54,7 +54,7 @@ gameOverSound = pygame.mixer.Sound('gameover.wav')
 pygame.mixer.music.load('background.mid')
 
 # Set up images.
-playerImage = pygame.image.load('mario_player.png')
+playerImage = pygame.image.load('pere-noel-image.png')
 playerRect = playerImage.get_rect()
 baddieImage = pygame.image.load('baddie_mario.png')
 
